4054ec3130cd6119230762516271126a3bf686f8
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 09:45:38 +0200

    Client creato

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -7,7 +13,24 @@ package com.example;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   
+        try{
+            Socket s = new Socket("localhost",3000);
+            BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream output = new DataOutputStream(s.getOutputStream());
+            System.out.println(" Creati input ed output");
+            output.writeBytes("ciao server \n");
+            System.out.println("stringa inviata");
+            String str = input.readLine();
+            System.out.println("stringa ricevuta in maiuscolo");
+            System.out.println(str);
+
+            s.close();
+
+        
+        }
+        catch(Exception e){
+
+        }
     }
 }
